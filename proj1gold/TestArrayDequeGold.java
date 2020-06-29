81dae4829a3e5b37325f16c68273b6a8296ed81f
Zhengliang Su <zhengliangsu@Zhengliangs-MacBook-Pro.local>
2020-06-29 13:42:26 -0700

    completed proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,21 +7,34 @@ public class TestArrayDequeGold {
     public void ArrayDequeTest() {
         /** @source: StudentArrayDequeLauncher.java */
 
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        String sequence = "\n";
 
         for (int i = 0; i < 100; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
             if (numberBetweenZeroAndOne < 0.25) {
-                sad1.addLast(i);
-                ads1.addLast(i);
+                sad.addFirst(i);
+                ads.addFirst(i);
+                sequence += String.format("addFirst(%s)\n", i);
+            } else if (numberBetweenZeroAndOne < 0.5) {
+                sad.addLast(i);
+                ads.addLast(i);
+                sequence += String.format("addLast(%s)\n", i);
+            } else if (sad.size() == 0 || ads.size() == 0) {
+                continue;
+            } else if (numberBetweenZeroAndOne < 0.75) {
+                Integer actual = sad.removeFirst();
+                Integer expected = ads.removeFirst();
+                sequence += "removeFirst()\n";
+                assertEquals(sequence, expected, actual);
             } else {
-                sad1.addFirst(i);
+                Integer actual = sad.removeLast();
+                Integer expected = ads.removeLast();
+                sequence += "removeLast()\n";
+                assertEquals(sequence, expected, actual);
             }
         }
-
-        sad1.printDeque();
-
     }
 }
