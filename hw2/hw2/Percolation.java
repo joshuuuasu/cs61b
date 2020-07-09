6771cc97bd1cd2473f2ae9489daea6d22ac7dea3
Zhengliang Su <zhengliangsu@Zhengliangs-MacBook-Pro.local>
2020-07-08 17:53:39 -0700

    finished hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,87 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import java.lang.*;
 
 public class Percolation {
 
+    private int[][] grid;
+    private int openSize;
+    private WeightedQuickUnionUF uf, uf2;
+
+    private int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N should be positive integer");
+        }
+        grid = new int[N][N];
+        openSize = 0;
+
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        uf2 = new WeightedQuickUnionUF(N * N + 1);
+        // uf2 is used for avoiding backwash (no bottom end point.)
+
+        for (int i = 1; i <= N; i++) {
+            uf.union(0, i);
+            uf2.union(0, i);
+        }
+        for (int i = N * N - N + 1; i <= N * N; i++) {
+            uf.union(N * N + 1, i);
+        }
+    }
+
+    public void open(int row, int col) {
+        if (row >= grid.length || col >= grid[0].length || row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException("index out of bounds for open()");
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        grid[row][col] = 1; // 1 denotes open, 0 (default) otherwise.
+        unionNeighbors(row, col);
+        openSize += 1;
+    }
+
+    private void unionNeighbors(int row, int col) {
+        for (int[] dir: dirs) {
+            int[] adj = {row + dir[0], col + dir[1]};
+            if (adj[0] >= 0 && adj[0] < grid.length && adj[1] >= 0 &&
+                    adj[1] < grid[0].length && grid[adj[0]][adj[1]] == 1) {
+                uf.union(xyTo1D(row, col), xyTo1D(adj[0], adj[1]));
+                uf2.union(xyTo1D(row, col), xyTo1D(adj[0], adj[1]));
+            }
+        }
+    }
+
+    private int xyTo1D(int row, int col) {
+        return row * grid[0].length + col + 1;
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row >= grid.length || col >= grid[0].length || row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException("index out of bounds for isOpen()");
+        }
+        return grid[row][col] == 1;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row >= grid.length || col >= grid[0].length || row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException("index out of bounds for isFull()");
+        }
+        return uf2.connected(0, xyTo1D(row, col)) && isOpen(row, col);
+    }
+
+    public int numberOfOpenSites() {
+        return openSize;
+    }
+
+    public boolean percolates() {
+        return uf.connected(0, grid.length * grid[0].length + 1);
+    }
+
+    public static void main(String[] args) {
+        Percolation x = new Percolation(10);
+        System.out.println(x.grid[0][0]);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,55 @@
 package hw2;
+import java.lang.*;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
 
+    private double mean;
+    private double sd;      //standard deviation
+    private double[] ci;    // confidence interval
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        double[] fraction = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int x = StdRandom.uniform(N * N + 1);
+                int[] pos = oneDToXY(x, N);
+                p.open(pos[0], pos[1]);
+            }
+            fraction[i] = ((double) p.numberOfOpenSites()) / (N * N);
+        }
+
+        mean = StdStats.mean(fraction);
+        sd = StdStats.stddev(fraction);
+        ci = new double[]{mean - 1.96 * sd / Math.sqrt(T), mean + 1.96 * sd / Math.sqrt(T)};
+    }
+
+    private int[] oneDToXY(int n, int colNum) {
+        int col = n % colNum;
+        int row = (n - col) / colNum;
+        int[] res = {row, col};
+        return res;
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return sd;
+    }
+
+    public double confidenceLow() {
+        return ci[0];
+    }
+
+    public double confidenceHigh() {
+        return ci[1];
+    }
 }
